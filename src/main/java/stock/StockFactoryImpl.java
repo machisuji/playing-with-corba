bb88e5b60d88565c3e4adb1218049d3d2a8d3668
Markus Kahl <machisuji@googlemail.com>
2012-03-17 17:44:42 +0100

    got the code to compile for starters

--- src/main/java/stock/StockFactoryImpl.java
+++ src/main/java/stock/StockFactoryImpl.java
@@ -6,28 +6,36 @@ import org.omg.CosNaming.NamingContextPackage.*;
 import org.omg.CORBA.*;
 import org.omg.PortableServer.*;
 import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAManagerPackage.State;
 
 public class StockFactoryImpl extends StockFactoryPOA {
 
     public Stock create_stock(String symbol, String description) {
-        org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init();
-        StockImpl newStock = new StockImpl(null, description);
+        try {
+            ORB orb = org.omg.CORBA.ORB.init();
+            StockImpl newStock = new StockImpl(null, description);
 
-        POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
-        if (rootpoa.the_POAManager().get_state() != POAManagerPackage.State.ACTIVE) {
-            rootpoa.the_POAManager().activate();
+            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            if (rootpoa.the_POAManager().get_state() != State.ACTIVE) {
+                rootpoa.the_POAManager().activate();
+            }
+            byte[] id = rootpoa.activate_object(newStock);
+
+            System.out.println("activated object: " + new String(id));
+
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(newStock);
+            Stock stockRef = StockHelper.narrow(ref);
+
+            register(stockRef, "stock/" + symbol, orb);
+
+            return stockRef;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Stock creation failed");
         }
-        newStock.setORB(orb);
-
-        org.omg.CORBA.Object ref = rootpoa.servant_to_reference(helloImpl);
-        Stock stockRef = StockHelper.narrow(ref);
-
-        register(stockRef, "stock/" + symbol);
-
-        return stockRef;
     }
 
-    protected void register(Stock stock, String name) {
+    protected void register(Stock stock, String name, ORB orb) throws Exception {
         org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
         NamingContextExt nc = NamingContextExtHelper.narrow(objRef);
         NameComponent path[] = nc.to_name(name);
